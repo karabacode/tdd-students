3f1592801f23e2823696c27fcc18f16c24794eb8
karabacode <luisvillab@gmail.com>
2020-07-07 14:51:57 +0200

    first commit with SpringBootApplication class

---
+++ src/main/java/org/karabacode/tddstudents/TddStudents.java
@@ -0,0 +1,13 @@
+package org.karabacode.tddstudents;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddStudents {
+    public static void main(String[] args){
+        SpringApplication.run(TddStudents.class);
+    }
+
+}
