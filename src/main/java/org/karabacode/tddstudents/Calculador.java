d20ffe48cee814ba388411b79d57b3205fc83c84
karabacode <luisvillab@gmail.com>
2020-07-07 15:16:38 +0200

    mínimo código para pasar el tests de suma

--- src/main/java/org/karabacode/tddstudents/Calculador.java
+++ src/main/java/org/karabacode/tddstudents/Calculador.java
@@ -1,4 +1,7 @@
 package org.karabacode.tddstudents;
 
 public class Calculador {
+    public int sum(int i, int i1) {
+        return 4;
+    }
 }
--- src/test/java/org/karabacode/tddstudents/test/CalculadorTests.java
+++ src/test/java/org/karabacode/tddstudents/test/CalculadorTests.java
@@ -3,6 +3,7 @@ package org.karabacode.tddstudents.test;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.karabacode.tddstudents.Calculador;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -13,7 +14,7 @@ public class CalculadorTests {
     @Test
     public void addTest(){
         Calculador calculador = new Calculador();
-        int result = calculador.add(2,2);
+        int result = calculador.sum(2,2);
         Assert.assertEquals(4,result);
     }
 }
